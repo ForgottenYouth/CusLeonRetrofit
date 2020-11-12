df343341b44c3bb6cf58b3b3b19e1bda03a865b5
shiwenliang <540658894@qq.com>
2020-11-12 09:14:15 +0800

    upload custom retrofit for annnotation ,reflect ,proxy

--- app/src/main/java/com/leon/leonretrofit/MainActivity.java
+++ app/src/main/java/com/leon/leonretrofit/MainActivity.java
@@ -6,6 +6,7 @@
 package com.leon.leonretrofit;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -15,6 +16,13 @@ import androidx.databinding.DataBindingUtil;
 import com.leon.leonretrofit.api.WeatherApi;
 import com.leon.leonretrofit.databinding.ActivityMainLayoutBinding;
 import com.leon.leonretrofit.retrofit.LeonRetrofit;
+
+import java.io.IOException;
+
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,13 +36,27 @@ public class MainActivity extends AppCompatActivity {
         mDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main_layout);
 
         //自定义的构造者模式来实例化
-        LeonRetrofit leonRetrofit = new LeonRetrofit.Builder("https://restapi.amap.com").build();
+        final LeonRetrofit leonRetrofit = new LeonRetrofit.Builder("https://restapi.amap.com").build();
         weatherApi = leonRetrofit.create(WeatherApi.class);
 
         mDataBinding.get.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                okhttp3.Call call = weatherApi.getWeather("110101", "ae6c53e2186f33bbf240a12d80672d1b");
+                call.enqueue(new okhttp3.Callback() {
 
+                    @Override
+                    public void onFailure(okhttp3.Call call, IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(okhttp3.Call call, Response response) throws IOException {
+                        Log.i("shiwenliang", "onResponse enjoy get: " + response.body().toString());
+                        response.close();
+                    }
+
+                });
             }
         });
 
--- app/src/main/java/com/leon/leonretrofit/api/WeatherApi.java
+++ app/src/main/java/com/leon/leonretrofit/api/WeatherApi.java
@@ -11,16 +11,14 @@ import com.leon.leonretrofit.retrofit.annotation.LEONGET;
 import com.leon.leonretrofit.retrofit.annotation.LEONPOST;
 import com.leon.leonretrofit.retrofit.annotation.LEONQuery;
 
-import okhttp3.ResponseBody;
-import retrofit2.Call;
 import retrofit2.http.FormUrlEncoded;
 
 public interface WeatherApi {
 
     @LEONPOST("/v3/weather/weatherInfo")
     @FormUrlEncoded
-    Call<ResponseBody> postWeather(@LEONField("city") String city, @LEONField("key") String key);
+    okhttp3.Call postWeather(@LEONField("city") String city, @LEONField("key") String key);
 
     @LEONGET("/v3/weather/weatherInfo")
-    Call<ResponseBody> getWeather(@LEONQuery("city") String city, @LEONQuery("key") String key);
+    okhttp3.Call getWeather(@LEONQuery("city") String city, @LEONQuery("key") String key);
 }
